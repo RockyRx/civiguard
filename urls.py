62f366c79e043f51eb06fcd629fa88715a7c313e
Oshadha Gunawardena <oshadha.rocky@gmail.com>
2015-07-25 20:27:55 +0530

    Fix module import typo in urls

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from testapplication import views
+from civiguardapp import views
 from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
